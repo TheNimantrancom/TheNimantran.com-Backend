6a0b07aae091044e6e04a9fb43b7e97c60bb89fd
ayushsingh212 <ayurajput249@gmail.com>
2025-11-27 01:05:29 +0530

    Removed the abstract because the express was treating it as a valid path

--- src/app.js
+++ src/app.js
@@ -7,16 +7,19 @@ import allRouter from "./routes/index.js";
 const app = express();
 
 
-const {URL1,URL2} = process.env
+const {FRONT1,FRONT2} = process.env
 
 const corsOptions = {
-  origin: [URL1,URL2],
+  origin: [FRONT1,FRONT2],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
-app.options("*", cors(corsOptions));
+
+
 app.use(cors(corsOptions));
+// app.options("*", (req, res) => res.sendStatus(200));
+
 
 app.use(express.json({ limit: "16mb" }));
 app.use(express.urlencoded({ extended: true, limit: "16mb" }));
