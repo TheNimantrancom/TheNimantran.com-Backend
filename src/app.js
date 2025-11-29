d537275ba5c044a58d9b1f30316e92a056dbf5fe
ayushsingh212 <ayurajput249@gmail.com>
2025-11-29 16:15:15 +0530

    Hard coded the local testing port for checking env variable loading issue

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 const {FRONT1,FRONT2} = process.env
 
 const corsOptions = {
-  origin: [FRONT1,FRONT2],
+  origin: ["http://localhost:3002",FRONT2],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
