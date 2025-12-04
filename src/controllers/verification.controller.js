e1e846e5ee87e582911046dddcba8979ca115e82
ayushsingh212 <ayurajput249@gmail.com>
2025-12-04 23:02:46 +0530

    Added the missing jwt

--- src/controllers/verification.controller.js
+++ src/controllers/verification.controller.js
@@ -6,6 +6,7 @@ import { sendEmail } from "../utils/sendMail.js";
 import { redisClient } from "../middlewares/otp.middleware.js";
 import { User }from "../models/user.model.js";
 // utils
+import jwt from "jsonwebtoken"
 export const generateOTP = () =>
   Math.floor(100000 + Math.random() * 900000).toString();
 
