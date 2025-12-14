0951ec3ba293239fc49a9ef16fdc9a5cc1859b5a
ayushsingh212 <ayurajput249@gmail.com>
2025-12-14 13:47:40 +0530

    Corrected the file path

--- src/controllers/admin/category.controller.js
+++ src/controllers/admin/category.controller.js
@@ -1,6 +1,6 @@
 import { Category } from "../../models/category.model.js";
 import  asyncHandler  from "../../utils/asyncHandler.js";
-import  ApiError  from "../../utils/ApiError.js";
+import  ApiError  from "../../utils/apiError.js";
 import  ApiResponse  from "../../utils/apiResponse.js"
 
 export const createCategory = asyncHandler(async (req, res) => {
