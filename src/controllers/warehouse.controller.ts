07aa466a926023f02f3bbd12830e2e08babfaaea
Ayush Singh <ayurajput249@gmail.com>
2026-03-10 23:48:41 +0530

    Fixed the typescript issue in the warhouse controller

--- src/controllers/warehouse.controller.ts
+++ src/controllers/warehouse.controller.ts
@@ -1,38 +1,47 @@
 import { Warehouse } from "../models/warehouse.model.js";
 import ApiResponse from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
+import { Request, Response } from "express";
+
+interface WarRequest extends Request {
+  query: {
+    lat: string
+    lng: string
+  }
+}
 
 export const getNearestWarehouse = asyncHandler(
-  async (req: Request, res: Response) => {
+  async (req:Request, res: Response)=>{
 
-    const lat = parseFloat(req.query.lat as string);
-    const lng = parseFloat(req.query.lng as string);
+    const lat = parseFloat(req?.query?.lat as any);
+    const lng = parseFloat(req?.query?.lng as any);
 
     if (!lat || !lng) {
-      throw new ApiError(400,"Coordinates required");
+      throw new ApiError(400, "Coordinates required");
     }
 
     const warehouses = await Warehouse.aggregate([
       {
-        $geoNear:{
-          near:{
-            type:"Point",
-            coordinates:[lng,lat]
+        $geoNear: {
+          near: {
+            type: "Point",
+            coordinates: [lng, lat]
           },
-          distanceField:"distance",
-          spherical:true
+          distanceField: "distance",
+          spherical: true
         }
       },
       {
-        $limit:1
+        $limit: 1
       }
     ]);
 
-    if(!warehouses.length){
-      res.json(
-        new ApiResponse(200,{serviceAvailable:false})
+    if (!warehouses.length) {
+     return  res.json(
+        new ApiResponse(200, { service: false }, "Fetched")
       );
-      return;
+     
     }
 
     const warehouse = warehouses[0];
@@ -40,17 +49,16 @@ export const getNearestWarehouse = asyncHandler(
     const withinRadius =
       warehouse.distance <= warehouse.deliveryRadius;
 
-    res.json(
-      new ApiResponse(200,{
-        serviceAvailable:withinRadius,
-        warehouse:{
-          id:warehouse._id,
-          name:warehouse.name,
-          city:warehouse.city,
-          distance:Math.round(warehouse.distance/1000)
+    return  res.status(200).json(
+      new ApiResponse(200, {
+        serviceAvailable: withinRadius,
+        warehouse: {
+          id: warehouse._id,
+          name: warehouse.name,
+          city: warehouse.city,
+          distance: Math.round(warehouse.distance / 1000)
         }
-      })
+      }, "Fetched successfully")
     );
-
   }
 );
